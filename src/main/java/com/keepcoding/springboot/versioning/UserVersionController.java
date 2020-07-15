28c296eab543026c9882858475317b5d59d2bce4
Jose Luis Solis <solis.jl@gmail.com>
2020-07-15 08:47:31 -0700

    Added versioning options

--- src/main/java/com/keepcoding/springboot/versioning/UserVersionController.java
+++ src/main/java/com/keepcoding/springboot/versioning/UserVersionController.java
@@ -15,4 +15,40 @@ public class UserVersionController {
 	public UserVersionV2 getUserVersion2() {
 		return new UserVersionV2(new NameStructure("nombre","apellido"));
 	}
+	
+	// PARAMS
+	
+	@GetMapping(value = "/userVersioning/params", params = "version=1")
+	public UserVersionV1 getUserVersion1Params() {
+		return new UserVersionV1("nombre");
+	}
+	
+	@GetMapping(value = "/userVersioning/params", params = "version=2")
+	public UserVersionV2 getUserVersion2Params() {
+		return new UserVersionV2(new NameStructure("nombre","apellido"));
+	}
+	
+	// HEADER
+	
+	@GetMapping(value = "/userVersioning/header", headers = "X-API-VERSION=1")
+	public UserVersionV1 getUserVersion1Header() {
+		return new UserVersionV1("nombre");
+	}
+	
+	@GetMapping(value = "/userVersioning/header", headers = "X-API-VERSION=2")
+	public UserVersionV2 getUserVersion2Header() {
+		return new UserVersionV2(new NameStructure("nombre","apellido"));
+	}
+	
+	// Content negociation
+	
+	@GetMapping(value = "/userVersioning/produces", produces = "application/vdn.company.app-v1+json")
+	public UserVersionV1 getUserVersion1Produces() {
+		return new UserVersionV1("nombre");
+	}
+	
+	@GetMapping(value = "/userVersioning/produces", produces = "application/vdn.company.app-v2+json")
+	public UserVersionV2 getUserVersion2Produces() {
+		return new UserVersionV2(new NameStructure("nombre","apellido"));
+	}
 }
