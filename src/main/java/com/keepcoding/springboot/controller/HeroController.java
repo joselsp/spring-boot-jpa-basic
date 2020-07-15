4ef3dc99498688311010c04769516520582bc14e
Jose Luis Solis <solis.jl@gmail.com>
2020-07-15 04:04:09 -0700

    Returned ResponseEntity in Hero creation

--- src/main/java/com/keepcoding/springboot/controller/HeroController.java
+++ src/main/java/com/keepcoding/springboot/controller/HeroController.java
@@ -1,13 +1,16 @@
 package com.keepcoding.springboot.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.keepcoding.springboot.dao.HeroDaoService;
 import com.keepcoding.springboot.model.Hero;
@@ -32,7 +35,12 @@ public class HeroController {
 	// Devolver estado 201: Created
 	// Devolver uri del nuevo recurso creado
 	@PostMapping("/hero")
-	public Hero addHero(@RequestBody Hero hero) {
-		return heroDaoService.addHero(hero);
+	public ResponseEntity<Object> addHero(@RequestBody Hero hero) {
+		Hero addedHero =  heroDaoService.addHero(hero);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(addedHero.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 }
