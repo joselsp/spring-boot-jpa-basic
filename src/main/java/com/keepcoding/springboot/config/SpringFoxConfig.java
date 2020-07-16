b532988f9d8abfb88562cf5ff4c442b809c17ce9
Jose Luis Solis <solis.jl@gmail.com>
2020-07-16 00:48:50 -0700

    Changed swagger version, added ApiInfo and validation swagger info

--- src/main/java/com/keepcoding/springboot/config/SpringFoxConfig.java
+++ src/main/java/com/keepcoding/springboot/config/SpringFoxConfig.java
@@ -1,16 +1,23 @@
 package com.keepcoding.springboot.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Import( BeanValidatorPluginsConfiguration.class )
 public class SpringFoxConfig {
 	
 	@Bean
@@ -19,7 +26,18 @@ public class SpringFoxConfig {
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.apiInfo(apiInfo());
 	}
-
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfo(
+				"My Rest Hero Api",
+				"Base de datos de SHIELD",
+				"API TOS",
+				"Terms of Service",
+				new Contact("me", "www.test.com", "example@test.com"),
+				"Licensed of API", "Api License url", Collections.emptyList()
+				);
+	}
 }
